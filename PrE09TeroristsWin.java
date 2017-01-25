17fa03da7c639129ce6dbdacc1b58ea3c389ec5a
Viktor Dimitrov <viktor.dimitrov.1989@gmail.com>
2017-01-25 16:14:39 +0200

    Terrorists win problem - modify

--- PrE09TeroristsWin.java
+++ PrE09TeroristsWin.java
@@ -4,36 +4,28 @@ public class PrE09TeroristsWin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
+        StringBuilder input = new StringBuilder(scanner.nextLine());
 
         while (true){
             int startIndex = input.indexOf("|");
-            if(startIndex == -1){
+            if(startIndex != - 1){
+                int endIndex = input.indexOf("|",startIndex + 1);
+                String bombContent = input.substring(startIndex + 1,endIndex);
+                int bombPower = calcPower(bombContent);
+                int start = Math.max(0,startIndex - bombPower);
+                int end = Math.min(input.length() - 1,endIndex + bombPower);
+                while (start <= end){
+                    input.setCharAt(start, '.');
+                    start++;
+                }
+            }
+            else{
                 break;
             }
-            int endIndex = input.indexOf("|", startIndex + 1) + 1;
-            String bomb = input.substring(startIndex + 1,endIndex - 1);
-            int power = calcPower(bomb);
-            if(endIndex + power > input.length() - 1){
-                endIndex = input.length();
-                power = 0;
-            }
-            if(startIndex - power < 0){
-                startIndex = 0;
-                power = 0;
-            }
-            String toReplace = input.substring(startIndex - power, endIndex + power);
-            input = input.replace(toReplace, repeat(toReplace));
-        }
-        System.out.println(input);
-    }
 
-    private static CharSequence repeat(String toReplace) {
-        String dot = ".";
-        for (int i = 0; i < toReplace.length() - 1; i++) {
-            dot += ".";
         }
-        return dot;
+        System.out.println(input.toString());
+
     }
 
     private static int calcPower(String bomb) {
